c766072276ddde3e34fe311c4bbb668ee8482c38
Rasel579 <Rasel579@gmail.com>
2021-09-23 00:36:16 +0500

    Changed Architecture of the App on MVVM (Deleted all realizations and abstractions for presentors), (Added supporting MVVM classes like ViewModel, ViewModelFactory), changed realization of UI, added to DI Dagger Module for ViewmodelsFactory and ViewModel. Added Observer of Internet connection (NetWorkStatus)

---
+++ app/src/main/java/com/professional/utils/NetworkStatus.kt
@@ -0,0 +1,10 @@
+package com.professional.utils
+
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+
+
+interface NetworkStatus {
+    fun isOnline(): Observable<Boolean>
+    fun onLineSingle(): Single<Boolean>
+}
